6aa0b3bfafd7dedbf81bee17c897bc2b2d605f36
Twilight0 <twilight@freemail.gr>
2020-06-29 19:24:57 +0300

    Zouzounia TV 3.0.0

--- addon.py
+++ addon.py
@@ -18,226 +18,31 @@
         along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
-import json
-from base64 import b64decode
-from tulip import youtube, directory, control, cache, bookmarks
-from tulip.init import sysaddon, syshandle
-from resources.lib import action, url
+from sys import argv
+from resources.lib import navigator
+from tulip.compat import parse_qsl
+from tulip import control, bookmarks, cache
 
-if control.setting('language') == '0':
-    if control.infoLabel('System.Language') == 'Greek':
-        main_id = 'UC9QSJuIBLUT2GbjgKymkTaQ'
-    elif control.infoLabel('System.Language') == 'Japanese':
-        main_id = 'UCGVTSfgmHJBzpq1gVq1ofhA'
-    else:
-        main_id = 'UCzsQf6eiWz4gIHgx0oYadXA'
-elif control.setting('language') == '2':
-    main_id = 'UC9QSJuIBLUT2GbjgKymkTaQ'
-elif control.setting('language') == '3':
-    main_id = 'UCGVTSfgmHJBzpq1gVq1ofhA'
-else:
-    main_id = 'UCzsQf6eiWz4gIHgx0oYadXA'
+params = dict(parse_qsl(argv[2].replace('?', '')))
 
-
-key = b64decode('QUl6YVN5QThrMU95TEdmMDNIQk5sMGJ5RDUxMWpyOWNGV28yR1I0')  # please do not copy this key
-
-
-def item_list():
-
-    return youtube.youtube(key=key).videos(main_id, limit=10)
-
-
-def _playlists():
-
-    return youtube.youtube(key=key).playlists(main_id, limit=10)
-
-
-def third_party():
-
-    menu = []
-
-    channels = [
-        {
-         'title': 'Πειραχτίρια TV',
-         'icon': 'https://yt3.ggpht.com/-tS9HnznNzaE/AAAAAAAAAAI/AAAAAAAAAAA/NaBbviI2fOk/s256-c-k-no-mo-rj-c0xffffff/photo.jpg',
-         'url': 'plugin://plugin.video.youtube/channel/UCnIEDKNRPQAsxU8gu0QqkaQ/'
-        }
-        ,
-        {
-         'title': 'Παιδικό Κανάλι',
-         'icon': 'https://yt3.ggpht.com/-1El1TLf3hlg/AAAAAAAAAAI/AAAAAAAAAAA/hUoR927dsaE/s256-c-k-no-mo-rj-c0xffffff/photo.jpg',
-         'url': 'plugin://plugin.video.youtube/channel/UCp_UGBizEZEoHJlS71ninSA/'
-        }
-        ,
-        {
-         'title': 'Kids Channel Network',
-         'icon': 'https://yt3.ggpht.com/-dR8D36X4sVM/AAAAAAAAAAI/AAAAAAAAAAA/TUV7N3Dg0CM/s256-c-k-no-mo-rj-c0xffffff/photo.jpg',
-         'url': 'plugin://plugin.video.youtube/channel/UCStBCJIgxvUXaggTo2C8ZQg/'
-        }
-        ,
-        {
-         'title': 'Παιδικά τραγούδια με στίχους',
-         'icon': 'https://yt3.ggpht.com/-Jdrq5I2r5Tc/AAAAAAAAAAI/AAAAAAAAAAA/z7IPqFS7jqA/s256-c-k-no-mo-rj-c0xffffff/photo.jpg',
-         'url': 'plugin://plugin.video.youtube/channel/UCyENiZwRYzfXzbwP-Mxk9oA/'
-        }
-        ,
-        {
-         'title': 'Disney Greece',
-         'icon': 'https://yt3.ggpht.com/-ONfYC2gl1wI/AAAAAAAAAAI/AAAAAAAAAAA/wURaWHLOI3k/s256-c-k-no-mo-rj-c0xffffff/photo.jpg',
-         'url': 'plugin://plugin.video.youtube/channel/UCy84vrlfPcEniv1wuyTr3fw/'
-        }
-    ]
-
-    for channel in channels:
-        li = control.item(label=channel['title'], iconImage=channel['icon'])
-        li.setArt({'fanart': control.addonInfo('fanart')})
-        li.addContextMenuItems([(control.lang(30006), 'RunPlugin({0}?action=addBookmark)'.format(sysaddon))])
-        url = channel['url']
-        isFolder = True
-        menu.append((url, li, isFolder))
-
-    control.addItems(syshandle, menu)
-    control.directory(syshandle)
-
-
-def playlists():
-
-    _list = cache.get(_playlists, 24)
-
-    for p in _list:
-        p.update({'action': 'youtube'})
-
-    for p in _list:
-        bookmark = dict((k, v) for k, v in p.iteritems() if not k == 'next')
-        bookmark['bookmark'] = p['url']
-        bm_cm = {'title': 30006, 'query': {'action': 'addBookmark', 'url': json.dumps(bookmark)}}
-        refresh = {'title': 30008, 'query': {'action': 'refresh'}}
-        cache_clear = {'title': 30005, 'query': {'action': 'cache_clear'}}
-        p.update({'cm': [refresh, cache_clear, bm_cm]})
-
-    directory.add(_list)
-
-
-def _youtube(plink):
-
-    _list = cache.get(youtube.youtube(key=key).playlist, 12, plink)
-
-    if _list is None:
-        return
-
-    for i in _list:
-        i.update({'action': 'play', 'isFolder': 'False'})
-
-    for item in _list:
-        bookmark = dict((k, v) for k, v in item.iteritems() if not k == 'next')
-        bookmark['bookmark'] = item['url']
-        bm_cm = {'title': 30006, 'query': {'action': 'addBookmark', 'url': json.dumps(bookmark)}}
-        refresh = {'title': 30008, 'query': {'action': 'refresh'}}
-        cache_clear = {'title': 30005, 'query': {'action': 'cache_clear'}}
-        item.update({'cm': [refresh, cache_clear, bm_cm]})
-
-    directory.add(_list)
-
-
-def videos():
-
-    video_list = cache.get(item_list, 12)
-
-    for v in video_list:
-        v.update({'action': 'play', 'isFolder': 'False'})
-
-    # for i in video_list:
-    #     i.update({'nextlabel': 30017, 'nextaction': 'videos'})
-
-    for item in video_list:
-        bookmark = dict((k, v) for k, v in item.iteritems() if not k == 'next')
-        bookmark['bookmark'] = item['url']
-        bm_cm = {'title': 30006, 'query': {'action': 'addBookmark', 'url': json.dumps(bookmark)}}
-        refresh = {'title': 30008, 'query': {'action': 'refresh'}}
-        cache_clear = {'title': 30005, 'query': {'action': 'cache_clear'}}
-        item.update({'cm': [refresh, cache_clear, bm_cm]})
-
-    directory.add(video_list)
-
-
-def bm_list():
-
-    bm = bookmarks.get()
-
-    na = [{'title': 30012, 'action': None, 'icon': 'not-found.jpg'}]
-
-    if not bm:
-        directory.add(na)
-        return na
-
-    for item in bm:
-        bookmark = dict((k, v) for k, v in item.iteritems() if not k == 'next')
-        bookmark['delbookmark'] = item['url']
-        item.update({'cm': [{'title': 30007, 'query': {'action': 'deleteBookmark', 'url': json.dumps(bookmark)}}]})
-
-    _list = sorted(bm, key=lambda k: k['title'].lower())
-
-    directory.add(_list)
-
-
-def main():
-
-    menu = [
-        {
-            'title': control.lang(30001),
-            'action': 'videos',
-            'icon': 'videos.jpg'
-        }
-        ,
-        {
-            'title': control.lang(30002),
-            'action': 'playlists',
-            'icon': 'playlists.jpg'
-        }
-        ,
-        {
-            'title': control.lang(30003),
-            'action': 'bookmarks',
-            'icon': 'heart.jpg'
-        }
-        ,
-        {
-            'title': control.lang(30013),
-            'action': 'third_party',
-            'icon': '3rd_party.jpg'
-        }
-        ,
-        {
-            'title': control.lang(30004),
-            'action': 'settings',
-            'icon': 'settings.jpg'
-        }
-    ]
-
-    if control.setting('third_party') == 'false':
-        del menu[-2]
-
-    cc = {'title': 30005, 'query': {'action': 'cache_clear'}}
-
-    for item in menu:
-        item.update({'cm': [cc]})
-
-    directory.add(menu)
-
-########################################################################################################################
+action = params.get('action')
+url = params.get('url')
+image = params.get('image')
+title = params.get('title')
+query = params.get('query')
 
 
 if action is None:
 
-    main()
+    navigator.Zouzounia().main()
 
 elif action == 'videos':
 
-    videos()
+    navigator.Zouzounia().videos()
 
 elif action == 'play':
 
-    directory.resolve(url)
+    navigator.Zouzounia().play(url)
 
 elif action == 'refresh':
 
@@ -245,19 +50,19 @@ elif action == 'refresh':
 
 elif action == 'playlists':
 
-    playlists()
+    navigator.Zouzounia().playlists()
 
 elif action == 'youtube':
 
-    _youtube(url)
+    navigator.Zouzounia().youtu(url)
 
 elif action == 'third_party':
 
-    third_party()
+    navigator.Zouzounia().third_party()
 
 elif action == 'bookmarks':
 
-    bm_list()
+    navigator.Zouzounia().bm_list()
 
 elif action == 'addBookmark':
 
@@ -275,7 +80,7 @@ elif action == 'cache_clear':
 
     if control.yesnoDialog(line1=control.lang(30009), line2='', line3=''):
 
-        control.deleteFile(control.cacheFile)
+        cache.clear(withyes=False)
         control.infoDialog(control.lang(30010))
 
     else:
---
+++ resources/lib/navigator.py
@@ -0,0 +1,199 @@
+# -*- coding: utf-8 -*-
+
+"""
+    Zouzounia TV Addon
+    Author: Twilight0
+
+        This program is free software: you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        (at your option) any later version.
+
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+
+        You should have received a copy of the GNU General Public License
+        along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import json
+from base64 import b64decode
+from tulip import youtube, directory, control, cache, bookmarks, client
+from tulip.compat import iteritems
+from youtube_resolver import resolve as resolver
+
+if control.setting('language') == '0':
+    if control.infoLabel('System.Language') == 'Greek':
+        main_id = 'UC9QSJuIBLUT2GbjgKymkTaQ'
+    elif control.infoLabel('System.Language') == 'Japanese':
+        main_id = 'UCGVTSfgmHJBzpq1gVq1ofhA'
+    else:
+        main_id = 'UCzsQf6eiWz4gIHgx0oYadXA'
+elif control.setting('language') == '2':
+    main_id = 'UC9QSJuIBLUT2GbjgKymkTaQ'
+elif control.setting('language') == '3':
+    main_id = 'UCGVTSfgmHJBzpq1gVq1ofhA'
+else:
+    main_id = 'UCzsQf6eiWz4gIHgx0oYadXA'
+
+
+key = b64decode('nZzbjNEWFNVLz5kakF3VUBDWrlkQ1lEVQJUYyp3VL9FR5NVY6lUQ'[::-1])  # please do not copy this key
+
+
+class Zouzounia:
+    
+    def __init__(self):
+
+        self.list = []
+
+    def main(self):
+
+        self.list = [
+            {
+                'title': control.lang(30001),
+                'action': 'videos',
+                'icon': 'videos.jpg'
+            }
+            ,
+            {
+                'title': control.lang(30002),
+                'action': 'playlists',
+                'icon': 'playlists.jpg'
+            }
+            ,
+            {
+                'title': control.lang(30003),
+                'action': 'bookmarks',
+                'icon': 'heart.jpg'
+            }
+            ,
+            {
+                'title': control.lang(30004),
+                'action': 'settings',
+                'icon': 'settings.jpg',
+                'isFolder': 'False',
+                'isPlayable': 'False'
+            }
+
+        ]
+
+        cc = {'title': 30005, 'query': {'action': 'cache_clear'}}
+
+        for item in self.list:
+            item.update({'cm': [cc]})
+
+        directory.add(self.list)
+    
+    def item_list(self):
+    
+        return youtube.youtube(key=key).videos(main_id, limit=10)
+
+    def _playlists(self):
+
+        return youtube.youtube(key=key).playlists(main_id, limit=10)
+
+    def playlists(self):
+    
+        self.list = cache.get(self._playlists, 24)
+    
+        for p in self.list:
+            p.update({'action': 'youtube'})
+
+        for p in self.list:
+            bookmark = dict((k, v) for k, v in iteritems(p) if not k == 'next')
+            bookmark['bookmark'] = p['url']
+            bm_cm = {'title': 30006, 'query': {'action': 'addBookmark', 'url': json.dumps(bookmark)}}
+            refresh = {'title': 30008, 'query': {'action': 'refresh'}}
+            cache_clear = {'title': 30005, 'query': {'action': 'cache_clear'}}
+            p.update({'cm': [refresh, cache_clear, bm_cm]})
+
+        directory.add(self.list)
+    
+    def youtu(self, plink):
+    
+        self.list = cache.get(youtube.youtube(key=key).playlist, 12, plink)
+
+        if self.list is None:
+            return
+
+        for v in self.list:
+            try:
+                title = v['title'].decode('utf-8')
+            except AttributeError:
+                title = v['title']
+            v.update({'action': 'play', 'isFolder': 'False', 'title': client.replaceHTMLCodes(title)})
+
+        for item in self.list:
+            bookmark = dict((k, v) for k, v in iteritems(item) if not k == 'next')
+            bookmark['bookmark'] = item['url']
+            bm_cm = {'title': 30006, 'query': {'action': 'addBookmark', 'url': json.dumps(bookmark)}}
+            refresh = {'title': 30008, 'query': {'action': 'refresh'}}
+            cache_clear = {'title': 30005, 'query': {'action': 'cache_clear'}}
+            item.update({'cm': [refresh, cache_clear, bm_cm]})
+
+        directory.add(self.list)
+    
+    def videos(self):
+    
+        self.list = cache.get(self.item_list, 12)
+
+        for v in self.list:
+            try:
+                title = v['title'].decode('utf-8')
+            except AttributeError:
+                title = v['title']
+            v.update({'action': 'play', 'isFolder': 'False', 'title': client.replaceHTMLCodes(title)})
+
+        for item in self.list:
+            bookmark = dict((k, v) for k, v in iteritems(item) if not k == 'next')
+            bookmark['bookmark'] = item['url']
+            bm_cm = {'title': 30006, 'query': {'action': 'addBookmark', 'url': json.dumps(bookmark)}}
+            refresh = {'title': 30008, 'query': {'action': 'refresh'}}
+            cache_clear = {'title': 30005, 'query': {'action': 'cache_clear'}}
+            item.update({'cm': [refresh, cache_clear, bm_cm]})
+    
+        directory.add(self.list)
+    
+    def bm_list(self):
+    
+        bm = bookmarks.get()
+
+        na = [{'title': 30012, 'action': None, 'icon': 'not-found.jpg'}]
+    
+        if not bm:
+            directory.add(na)
+            return na
+
+        for item in bm:
+            bookmark = dict((k, v) for k, v in iteritems(item) if not k == 'next')
+            bookmark['delbookmark'] = item['url']
+            item.update({'cm': [{'title': 30007, 'query': {'action': 'deleteBookmark', 'url': json.dumps(bookmark)}}]})
+    
+        self.list = sorted(bm, key=lambda k: k['title'].lower())
+
+        directory.add(self.list)
+
+    @staticmethod
+    def session(link):
+
+        streams = resolver(link)
+
+        try:
+            addon_enabled = control.addon_details('inputstream.adaptive').get('enabled')
+        except KeyError:
+            addon_enabled = False
+
+        if not addon_enabled:
+            streams = [s for s in streams if 'mpd' not in s['title'].lower()]
+
+        stream = streams[0]['url']
+
+        return stream
+
+    def play(self, url):
+
+        stream = self.session(url)
+
+        directory.resolve(stream, dash='.mpd' in stream)
